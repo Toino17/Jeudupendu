65bb74dad83e671852ab6d99f29a13e161ea5b99
Toino17 <colonnact@outlook.com>
2023-09-21 01:53:59 +0200

    21/09

--- pendu.js
+++ pendu.js
@@ -1,5 +1,4 @@
 let data = [
-            "abandonner",
             "abandonné",
             "abattoir",
             "abattu",
@@ -42,6 +41,13 @@ let data = [
 
 let playbutton = document.querySelector(".play")
 let UnderGuess = ""
+let baliseHistory = document.querySelector(".historyP")
+let historia = "Historique: "
+let reg = /^[a-zA-Zé]$/
+let attempt=1
+let alreadyUse = 0
+let erreur = document.querySelector(".erreur")
+
 
 function searchRandomWord(){
    let randomNumber = Math.floor((Math.random() * data.length))
@@ -64,33 +70,87 @@ function ShowUnder(){
     baliseDevine.innerHTML = UnderGuess
 }
 
+function history(){
+   indexstory = historia.indexOf(event.key.toLowerCase(), 11)
+   let erreur = document.querySelector(".erreur")
+   if (indexstory ==-1){
+    erreur.style.visibility = "hidden"
+    erreur.innerHTML = ""
+    historia += event.key.toLowerCase()
+    baliseHistory.innerHTML = historia
+    return alreadyUse = 0
+   }
+   else{
+    erreur.innerHTML = `Lettre ${event.key.toUpperCase()} déjà utilisée`
+    erreur.style.visibility = "visible"
+    erreur.id = "erreur"
+    setTimeout(function anim (){erreur.id=""}, 500)
+    return alreadyUse = 1
+   }
+}
+
+function reset(){
+    baliseHistory.innerHTML = "Historique: "
+    historia = "Historique: "
+}
+
 
 
 playbutton.addEventListener("click", function() {
     event.preventDefault()
+    reset()
     searchRandomWord()
     dataToUnder(randomWord)
     ShowUnder()
   }); 
 
 document.addEventListener("keydown",function(event){
-    let index = randomWord.indexOf(event.key)
-    if(index !==-1){
-        document.body.style.backgroundColor="red"
-        randomWord.replace()
-    }
-    else{
-        alert("pas de lettre")
-    }
-    console.log(event)
-    console.log(index)
-})
-
+    if (reg.test(event.key) == true ) {
+        caracKey = (event.key.toLowerCase())
+        history()
+        let pos = randomWord.indexOf(caracKey)
+        let count = 0
+        let ind = -1
+        while (pos != -1) {
+            count++;
+            pos = randomWord.indexOf(caracKey, pos + 1);
+        }
+        if(count > 0){
+            for (let i = 0; i < count; i++) {
+                    console.log(i)
+                    console.log("index:",ind,"i", i, "=", ind+i)
+                    ind = randomWord.indexOf(caracKey, ind+1)
+                    console.log("index: ",ind)
+                    console.log(UnderGuess)
+                    UnderGuess = UnderGuess.replaceAt(ind, caracKey)      
+            }
+        }
+        else if (alreadyUse==0){
+            attempt++
+            document.getElementById(`rect${attempt}`).style.display = "block"
+        }
+        else{
+            return
+        }
+        
+        UnderGuess = UnderGuess.charAt(0).toUpperCase() + UnderGuess.slice(1)
+        ShowUnder()
+        console.log(randomWord)
+        console.log(count)
+        console.log(UnderGuess)
+        console.log(UnderGuess.charAt(0).toUpperCase())
+        console.log(UnderGuess.slice(1))
+}   }   )
+   
 String.prototype.replaceAt = function(index, replacement) {
-    return this.substr(0, index) + replacement
-        + this.substr(index + replacement.length);
+    if (index >= this.length) {
+        return this.valueOf();
+    }
+    return this.substring(0, index) + replacement + this.substring(index + 1);
 }
 
-newRandomWord
+
+ 
+
 
 // document.body.style.backgroundColor="red"
